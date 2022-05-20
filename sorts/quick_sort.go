103effd0316c5c66bcacf189f049bff3a9e048ce
Hu Xulm <huxulm@gmail.com>
2022-05-20 17:55:20 +0800

    update quick_sort.go

--- sorts/quick_sort.go
+++ sorts/quick_sort.go
@@ -1,5 +1,10 @@
 package sorts
 
+import (
+	"math"
+	"math/rand"
+)
+
 func partition(arr []int, lo, hi int) int {
 	index := lo - 1
 	pivot := hi
@@ -17,9 +22,20 @@ func quickSortRange(arr []int, lo, hi int) {
 	if lo >= hi {
 		return
 	}
-	pivot := partition(arr, lo, hi)
+	pivot := randomPartition(arr, lo, hi)
 	quickSortRange(arr, lo, pivot-1)
 	quickSortRange(arr, pivot+1, hi)
+}
+
+func init() {
+	rand.Seed(math.MaxInt64)
+}
+
+//《算法导论》9.2：期望为线性的选择算法
+func randomPartition(arr []int, lo, hi int) int {
+	pivot := rand.Int()%(hi-lo+1) + lo
+	arr[pivot], arr[hi] = arr[hi], arr[pivot]
+	return partition(arr, lo, hi)
 }
 
 // QuickSort Sorts the entire array
